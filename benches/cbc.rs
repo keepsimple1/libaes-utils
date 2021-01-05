c8db230caae1799d41d84a9e3b6a773b065c7f12
Han Xu <keepsimple@gmail.com>
2021-01-05 13:51:40 -0800

    Initial implementation

---
+++ benches/cbc.rs
@@ -0,0 +1,65 @@
+use aes::Aes128;
+use block_modes::block_padding::Pkcs7;
+use block_modes::{BlockMode, Cbc};
+use criterion::{criterion_group, criterion_main, Criterion};
+use libaes::Cipher;
+
+/// This bench suite compares the `libaes` crate and `block-modes` + `aes` crates,
+/// mainly on `encrypt` and `decrypt` performance for AES128 CBC mode, both using
+/// Pkcs7 padding.
+///
+/// The code using `block-modes` and `aes` mimics the example in their online doc.
+
+// create an alias for convenience
+type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+
+const SMALL_PLAIN_TEXT: &[u8; 50] = b"r97dXKLmEOsn4o3ZDDn2xowXG4XblpKuQrbnDvazCYx4O8iNpq";
+const SMALL_CIPHER_TEXT: &[u8; 64] =
+    b"\x15\x1e\x11\x15\xf5\x0f\x90\x37\xc9\xc6\x2f\x11\x13\xb3\x1c\xa1\
+         \x28\x89\x12\xdc\xe6\x4a\xa9\x32\xc8\xa4\x3f\x61\xc4\xcf\x88\xf0\
+         \x98\xad\xf1\x02\x2d\xd2\xc8\x06\x51\x00\x5a\x11\x88\x2e\xdb\xc5\
+         \x8f\x67\xa9\x09\x68\xfb\x0a\xaa\xfe\xd7\xa7\x69\x6f\x62\x2f\x25";
+const SMALL_KEY: &[u8; 16] = b"This is our key!";
+
+fn libaes_128_cbc_encrypt() {
+    let iv = b"This is 16 bytes";
+    let cipher = Cipher::new_128(SMALL_KEY);
+    cipher.cbc_encrypt(iv, SMALL_PLAIN_TEXT);
+}
+
+fn libaes_128_cbc_decrypt() {
+    let iv = b"This is 16 bytes";
+    let cipher = Cipher::new_128(SMALL_KEY);
+    let decrypted = cipher.cbc_decrypt(iv, SMALL_CIPHER_TEXT).unwrap();
+    assert_eq!(&decrypted[..], &SMALL_PLAIN_TEXT[..]);
+}
+
+fn aes_block_modes_128_cbc_encrypt() {
+    let iv = b"This is 16 bytes";
+    let cipher = Aes128Cbc::new_var(SMALL_KEY, iv).unwrap();
+    cipher.encrypt_vec(SMALL_PLAIN_TEXT);
+}
+
+fn aes_block_modes_128_cbc_decrypt() {
+    let iv = b"This is 16 bytes";
+    let cipher = Aes128Cbc::new_var(SMALL_KEY, iv).unwrap();
+    cipher.decrypt_vec(SMALL_CIPHER_TEXT).unwrap();
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("libaes 128 cbc encrypt", |b| {
+        b.iter(|| libaes_128_cbc_encrypt())
+    });
+    c.bench_function("aes-block-modes 128 cbc encrypt", |b| {
+        b.iter(|| aes_block_modes_128_cbc_encrypt())
+    });
+    c.bench_function("libaes 128 cbc decrypt", |b| {
+        b.iter(|| libaes_128_cbc_decrypt())
+    });
+    c.bench_function("aes-block-modes 128 cbc decrypt", |b| {
+        b.iter(|| aes_block_modes_128_cbc_decrypt())
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ src/main.rs
@@ -0,0 +1,169 @@
+use base64::{decode, encode};
+use clap::Arg;
+use clap::SubCommand;
+use libaes::Cipher;
+use std::convert::TryInto;
+use std::fs::File;
+use std::io::{Read, Write};
+
+fn main() {
+    let matches = clap::App::new("lau")
+        .version("0.1.0")
+        .subcommand(
+            SubCommand::with_name("encrypt")
+                .about("AES 128 encrypt in CBC mode, encrypt -h for more details")
+                .arg(
+                    Arg::with_name("key")
+                        .help("must be 16 characters")
+                        .short("k")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("iv")
+                        .help("must be 16 characters")
+                        .short("i")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("file")
+                        .help("file name to write cipher text")
+                        .short("f")
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("message")
+                        .help("plain text")
+                        .short("m")
+                        .required(true)
+                        .takes_value(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("decrypt")
+                .about("AES 128 decrypt in CBC mode, decrypt -h for more details")
+                .arg(
+                    Arg::with_name("key")
+                        .help("must be 16 characters")
+                        .short("k")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("iv")
+                        .help("must be 16 characters")
+                        .short("i")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("file")
+                        .help("file name to read cipher text")
+                        .conflicts_with("message")
+                        .required(true)
+                        .short("f")
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("message")
+                        .help("Cipher text in BASE64")
+                        .conflicts_with("file")
+                        .required(true)
+                        .short("m")
+                        .required(true)
+                        .takes_value(true),
+                ),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        ("encrypt", Some(sub_m)) => {
+            let key = sub_m.value_of("key").unwrap();
+            if key.len() != libaes::AES_128_KEY_LEN {
+                println!("key must be {} characters long", libaes::AES_128_KEY_LEN);
+                return;
+            }
+            let iv = sub_m.value_of("iv").unwrap();
+            if iv.len() != 16 {
+                println!("iv must be 16 characters long");
+                return;
+            }
+            let msg = sub_m.value_of("message").unwrap();
+            let cipher = Cipher::new_128(key.as_bytes().try_into().unwrap());
+            let cipher_bytes = cipher.cbc_encrypt(iv.as_bytes(), msg.as_bytes());
+
+            if let Some(file_name) = sub_m.value_of("file") {
+                let mut file = match File::create(file_name) {
+                    Ok(f) => f,
+                    Err(e) => {
+                        println!("Failed to create file: {}", e);
+                        return;
+                    }
+                };
+                if let Err(e) = file.write_all(&cipher_bytes[..]) {
+                    println!("Failed to write file: {}", e);
+                    return;
+                }
+                println!("Cipher text written into file: {}", file_name);
+            } else {
+                println!("Cipher text in BASE64:\n{}", encode(cipher_bytes));
+            }
+        }
+        ("decrypt", Some(sub_m)) => {
+            let key = sub_m.value_of("key").unwrap();
+            if key.len() != libaes::AES_128_KEY_LEN {
+                println!("key must be {} characters long", libaes::AES_128_KEY_LEN);
+                return;
+            }
+            let iv = sub_m.value_of("iv").unwrap();
+            if iv.len() != 16 {
+                println!("iv must be 16 characters long");
+                return;
+            }
+
+            // read cipher text from "message" or "file"
+            let msg = match sub_m.value_of("message") {
+                Some(msg_base64) => match decode(msg_base64) {
+                    Ok(m) => m,
+                    Err(e) => {
+                        println!("Failed to decode Base64: {}", e);
+                        return;
+                    }
+                },
+                None => match sub_m.value_of("file") {
+                    Some(file_name) => {
+                        let mut msg = vec![];
+                        match File::open(file_name) {
+                            Ok(mut f) => f.read_to_end(&mut msg).unwrap(),
+                            Err(e) => {
+                                println!("Failed to open file: {}", e);
+                                return;
+                            }
+                        };
+                        msg
+                    }
+                    None => {
+                        println!("Missing file name to read cipher text");
+                        return;
+                    }
+                },
+            };
+
+            // decrypt the cipher text
+            let cipher = Cipher::new_128(key.as_bytes().try_into().unwrap());
+            let plain_bytes = match cipher.cbc_decrypt(iv.as_bytes(), &msg[..]) {
+                Ok(bytes) => bytes,
+                Err(e) => {
+                    println!("Failed: {}", e);
+                    return;
+                }
+            };
+            match std::str::from_utf8(&plain_bytes[..]) {
+                Ok(s) => println!("{}", s),
+                Err(_) => println!("Plain text is not a string, cannot print it"),
+            }
+        }
+        _ => {}
+    }
+}
